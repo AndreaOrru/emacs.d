571eb6741f73aa1d2b269107648b3b96b5fcb875
Andrea Orru <andreaorru1991@gmail.com>
2016-03-14 14:32:24 +0000

    Apropos do all, and enable all functions.

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -4,7 +4,11 @@
 ;;;   Small adjustments to various things.
 
 ;;; Code:
-(fset 'yes-or-no-p 'y-or-n-p)  ; Y/N instead of yes/no.
+(require 'apropos)
+
+(fset 'yes-or-no-p 'y-or-n-p)         ; Y/N instead of yes/no.
+(setq apropos-do-all t)               ; Show more informations.
+(setq disabled-command-function nil)  ; Enable all functions.
 
 (provide 'init-misc)
 ;;; init-misc.el ends here
