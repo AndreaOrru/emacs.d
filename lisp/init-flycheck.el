9a3b6e13063fb473783a4c0b526a8fa6a1591437
Andrea Orru <andreaorru1991@gmail.com>
2016-03-18 20:47:42 +0000

    Tooltips for Flycheck.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,8 +6,10 @@
 ;;; Code:
 (require 'require-package)
 (require-package 'flycheck)
+(require-package 'flycheck-pos-tip)
 
 (global-flycheck-mode 1)
+(flycheck-pos-tip-mode)   ; Show errors in tooltips.
 ; Set the current Emacs load path in Flycheck:
 (setq-default flycheck-emacs-lisp-load-path load-path)
 
