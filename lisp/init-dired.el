021af0e85e022ef2ee9a07f139d65cc9a43a0c8e
Andrea Orru <andreaorru1991@gmail.com>
2016-04-17 23:32:44 +0100

    Dired in a single buffer.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -4,6 +4,17 @@
 ;;;   Configure Dired.
 
 ;;; Code:
+(require 'require-package)
+
+; Use just a single buffer:
+(require-package 'dired-single)
+
+(add-hook 'dired-mode-hook '(lambda()
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map "^"
+    (function
+      (lambda nil (interactive) (dired-single-buffer ".."))))))
 
 ; Show VCS differences:
 (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
