3c90a429aadc0eac392bd88fa28a81a062a78c48
Andrea Orru <andreaorru1991@gmail.com>
2016-04-22 03:29:59 +0100

    Fixed Javascript indentation and autocompletion.

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -11,10 +11,14 @@
 (add-to-list 'auto-mode-alist  '("\\.jsx?\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 
-(setq-default js2-basic-offset 2)
-(add-hook 'js2-jsx-mode-hook (lambda()
+(add-hook 'js2-jsx-mode-hook '(lambda()
+  (setq js2-basic-offset 2)
   ; Fast switch to web-mode (for JSX's HTML tags):
   (local-set-key (kbd "C-c m") 'web-mode)))
+
+; Activate autocompletion in js2-jsx-mode:
+(require 'auto-complete-config)
+(add-to-list 'ac-modes 'js2-jsx-mode)
 
 (provide 'init-javascript)
 ;;; init-javascript.el ends here
