dce9a6fff3bc5a519c453785de92c5ebecc26413
Andrea Orru <andreaorru1991@gmail.com>
2016-05-11 14:43:22 +0100

    Whitespace cleanup revised.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -16,15 +16,17 @@
 (if window-system (global-hl-line-mode t))
 
 (setq-default
-  auto-save-default nil    ; Don't auto save files.
-  column-number-mode t     ; Show column number in status bar.
-  indent-tabs-mode nil     ; Don't use tabs to indent.
-  make-backup-files nil    ; Don't create backup files.
-  require-final-newline t  ; Add final newline on saving.
-  tab-width 4)             ; Tabs displayed as 4 spaces.
+  auto-save-default nil       ; Don't auto save files.
+  column-number-mode t        ; Show column number in status bar.
+  indent-tabs-mode nil        ; Don't use tabs to indent.
+  make-backup-files nil       ; Don't create backup files.
+  require-final-newline t     ; Add final newline on saving.
+  show-trailing-whitespace t  ; Show trailing whitespaces.
+  tab-width 4)                ; Tabs displayed as 4 spaces.
 
 ; Delete trailing whitespaces on saving:
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(require-package 'whitespace-cleanup-mode)
+(global-whitespace-cleanup-mode t)
 
 ; Insert line break and then indent the new line:
 (global-set-key (kbd "RET") 'newline-and-indent)
