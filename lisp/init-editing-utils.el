8cc81f0c0b1ac1a2b663b71c0d84206cb80b4336
Andrea Orru <andreaorru1991@gmail.com>
2016-07-19 02:02:51 +0200

    Simplify multiple cursors.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -17,12 +17,13 @@
 (if window-system (global-hl-line-mode 1))
 
 (setq-default
-  auto-save-default nil    ; Don't auto save files.
-  column-number-mode t     ; Show column number in status bar.
-  indent-tabs-mode nil     ; Don't use tabs to indent.
-  make-backup-files nil    ; Don't create backup files.
-  require-final-newline t  ; Add final newline on saving.
-  tab-width 4)             ; Tabs displayed as 4 spaces.
+  auto-save-default nil     ; Don't auto save files.
+  delete-active-region nil  ; Don't delete selection with backspace.
+  column-number-mode t      ; Show column number in status bar.
+  indent-tabs-mode nil      ; Don't use tabs to indent.
+  make-backup-files nil     ; Don't create backup files.
+  require-final-newline t   ; Add final newline on saving.
+  tab-width 4)              ; Tabs displayed as 4 spaces.
 
 ; Delete trailing whitespaces on saving:
 (require-package 'whitespace-cleanup-mode)
@@ -36,6 +37,7 @@
 
 ; Progressive expansion of selected region:
 (require-package 'expand-region)
+(setq expand-region-fast-keys-enabled nil)
 (global-set-key (kbd "C->") 'er/expand-region)
 (global-set-key (kbd "C-<") 'er/contract-region)
 
