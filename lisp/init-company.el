fd3e57f27a4971fc7d18adcfe87fa2cec113af24
Andrea Orru <andreaorru1991@gmail.com>
2016-07-22 07:23:28 +0100

    Company try hard. Restored multiple cursors shortcuts.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -7,12 +7,13 @@
 (require 'require-package)
 (require-package 'company)
 (require-package 'company-quickhelp)
+(require-package 'company-try-hard)
 (require 'company)
 
 (global-company-mode 1)
 (company-quickhelp-mode 1)
 
-(define-key company-mode-map (kbd "M-TAB") 'company-complete)
+(define-key company-mode-map (kbd "M-TAB") 'company-try-hard)
 (define-key company-active-map (kbd "M-TAB") 'company-select-next)
 (define-key company-active-map (kbd "M-h") 'company-quickhelp-mode)
 
--- lisp/init-multiple-cursors.el
+++ lisp/init-multiple-cursors.el
@@ -11,6 +11,10 @@
 (global-set-key (kbd "C-S-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-S-p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-S-l") 'mc/mark-all-like-this)
+; Commands that start from the word on point:
+(global-set-key (kbd "M-N") 'mc/mark-next-like-this-word)
+(global-set-key (kbd "M-P") 'mc/mark-previous-like-this-word)
+(global-set-key (kbd "M-L") 'mc/mark-all-words-like-this)
 ; Add cursors by clicking:
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
