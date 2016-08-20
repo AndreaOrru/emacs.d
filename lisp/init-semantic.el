e572486315db8547c47c96e28226d7e562474095
Andrea Orru <andreaorru1991@gmail.com>
2016-08-20 15:56:13 +0100

    semantic-parse-dir.

--- lisp/init-semantic.el
+++ lisp/init-semantic.el
@@ -17,11 +17,29 @@
   (add-to-list 'semantic-inhibit-functions (lambda()
     (equal major-mode 'python-mode))))
 
-; Force loading all the includes:
+(semantic-mode 1)  ; Enable Semantic globally.
+
+; Use info extracted from Irony to help Semantic:
+(require 'irony)
+(defadvice irony-cdb--update-compile-options (after my/irony-cdb--update-compile-options activate)
+  "Pass the include paths detected by Irony to Semantic."
+  (dolist (dir (irony--extract-user-search-paths
+                irony--compile-options
+                irony--working-directory))
+    (semantic-add-system-include dir)))
+
+; Force reloading all the includes after Irony update:
 (add-to-list 'semantic-init-db-hook (lambda()
   (semanticdb-find-translate-path nil nil)))
 
-(semantic-mode 1)  ; Enable Semantic globally.
+(defun semantic-parse-dir (root)
+  "Make Semantic parse all source files in directory ROOT, recursively."
+  (interactive (list (read-directory-name "Root directory; "
+                                          irony--working-directory)))
+  (dolist (file (directory-files-recursively
+                 root
+                 ".*\\.\\(c\\|cpp\\|cxx\\|h\\|hpp\\|hxx\\)"))
+    (semanticdb-file-table-object file)))
 
 (provide 'init-semantic)
 ;;; init-semantic.el ends here
