0edbc34f1d90267f33ebc5aa16c9add3abfd9263
Andrea Orru <andreaorru1991@gmail.com>
2016-08-21 19:33:24 +0100

    Remove mouse case from dired.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -10,8 +10,7 @@
 (require-package 'dired-single)
 
 (add-hook 'dired-mode-hook '(lambda()
-  (define-key dired-mode-map [return] 'dired-single-buffer)
-  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map (kbd "RET") 'dired-single-buffer)
   (define-key dired-mode-map "^"
     (function
       (lambda nil (interactive) (dired-single-buffer ".."))))))
